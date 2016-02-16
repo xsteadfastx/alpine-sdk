63f92db71a87ff892c30d2dee061ffa70fd3362c
Marvin Steadfast <marvin@xsteadfastx.org>
2016-02-16 15:53:34 +0100

    fixed abuild situation

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -ti --rm -v $PWD/keys:/etc/apk/keys -v $PWD/aports:/home/marv/aports -v $PWD:/data alpine-sdk sh
+docker run -ti --rm -v $PWD/keys:/etc/apk/keys -v $PWD/aports:/home/marv/aports -v $PWD/abuild:/home/marv/.abuild -v $PWD/abuild.conf:/etc/abuild.conf -v $PWD:/data alpine-sdk sh
